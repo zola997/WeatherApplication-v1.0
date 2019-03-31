2d6e1c56cdbf1981dab923f1a60a7ea85bea22d3
Laza <laza9977@gmail.com>
2019-03-31 22:40:50 +0200

    started v2.0

--- app/src/main/java/pnrs/vezbe/projekat_1/MainActivity.java
+++ app/src/main/java/pnrs/vezbe/projekat_1/MainActivity.java
@@ -1,15 +1,24 @@
 package pnrs.vezbe.projekat_1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    RadioButton button;
+
 
     Button b1;
     EditText editText;
@@ -19,9 +28,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText=findViewById(R.id.editText);
-        textView=findViewById(R.id.textView2);
-        b1 = findViewById(R.id.button);
+
+        Context context = (Context) this;
+        ArrayList<String> cities;
+        View view;
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(R.layout.element_row,null);
+        ViewHolder viewHolder;
+
+
+
+
+        // editText=findViewById(R.id.editText);
+       // textView=findViewById(R.id.textView2);
+       // b1 = findViewById(R.id.button);
 
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
